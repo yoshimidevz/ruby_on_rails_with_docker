e8d19946a0f840dbed3a7c858e3962940f1ab521
yoshimidevz <yoshimidev1@gmail.com>
2025-10-30 16:12:50 -0300

    feat: enhance Docker setup and add Tailwind CSS configuration with new styles and quiz view

---
+++ meu_app/config/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/views/**/*.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
